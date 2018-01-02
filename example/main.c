d3db673a9256c9667eb87985ba3c0b729edf7771
彭强兵 <pengqiangbing@pengqiangbingdeMacBook.local>
2018-01-02 16:53:44 +0800

    format main.c

--- example/main.c
+++ example/main.c
@@ -23,36 +23,36 @@ int kq;
 int sockfd;
 
 char html[] = 
-"HTTP/1.1 200 OK\r\n"
-"Server: F-Stack\r\n"
-"Date: Sat, 25 Feb 2017 09:26:33 GMT\r\n"
-"Content-Type: text/html\r\n"
-"Content-Length: 439\r\n"
-"Last-Modified: Tue, 21 Feb 2017 09:44:03 GMT\r\n"
-"Connection: keep-alive\r\n"
-"Accept-Ranges: bytes\r\n"
-"\r\n"
-"<!DOCTYPE html>\r\n"
-"<html>\r\n"
-"<head>\r\n"
-"<title>Welcome to F-Stack!</title>\r\n"
-"<style>\r\n"
-"    body {  \r\n"
-"        width: 35em;\r\n"
-"        margin: 0 auto; \r\n"
-"        font-family: Tahoma, Verdana, Arial, sans-serif;\r\n"
-"    }\r\n"
-"</style>\r\n"
-"</head>\r\n"
-"<body>\r\n"
-"<h1>Welcome to F-Stack!</h1>\r\n"
-"\r\n"
-"<p>For online documentation and support please refer to\r\n"
-"<a href=\"http://F-Stack.org/\">F-Stack.org</a>.<br/>\r\n"
-"\r\n"
-"<p><em>Thank you for using F-Stack.</em></p>\r\n"
-"</body>\r\n"
-"</html>";
+"HTTP/1.1 200 OK\r\n";
+//"Server: F-Stack\r\n"
+//"Date: Sat, 25 Feb 2017 09:26:33 GMT\r\n"
+//"Content-Type: text/html\r\n"
+//"Content-Length: 439\r\n"
+//"Last-Modified: Tue, 21 Feb 2017 09:44:03 GMT\r\n"
+//"Connection: keep-alive\r\n"
+//"Accept-Ranges: bytes\r\n"
+//"\r\n"
+//"<!DOCTYPE html>\r\n"
+//"<html>\r\n"
+//"<head>\r\n"
+//"<title>Welcome to F-Stack!</title>\r\n"
+//"<style>\r\n"
+//"    body {  \r\n"
+//"        width: 35em;\r\n"
+//"        margin: 0 auto; \r\n"
+//"        font-family: Tahoma, Verdana, Arial, sans-serif;\r\n"
+//"    }\r\n"
+//"</style>\r\n"
+//"</head>\r\n"
+//"<body>\r\n"
+//"<h1>Welcome to F-Stack!</h1>\r\n"
+//"\r\n"
+//"<p>For online documentation and support please refer to\r\n"
+//"<a href=\"http://F-Stack.org/\">F-Stack.org</a>.<br/>\r\n"
+//"\r\n"
+//"<p><em>Thank you for using F-Stack.</em></p>\r\n"
+//"</body>\r\n"
+//"</html>";
 
 int loop(void *arg)
 {
@@ -66,10 +66,8 @@ int loop(void *arg)
 
         /* Handle disconnect */
         if (event.flags & EV_EOF) {
-
             /* Simply close socket */
             ff_close(clientfd);
-
             printf("A client has left the server...,fd:%d\n", clientfd);
         } else if (clientfd == sockfd) {
             int nclientfd = ff_accept(sockfd, NULL, NULL);
@@ -79,12 +77,7 @@ int loop(void *arg)
             }
 
             /* Add to event list */
-            kevSet.data     = 0;
-            kevSet.fflags   = 0;
-            kevSet.filter   = EVFILT_READ;
-            kevSet.flags    = EV_ADD;
-            kevSet.ident    = nclientfd;
-            kevSet.udata    = NULL;
+            EV_SET(&kevSet, nclientfd, EVFILT_READ, EV_ADD, 0, 0, NULL);
 
             assert(ff_kevent(kq, &kevSet, 1, NULL, 0, NULL) == 0);
 
@@ -93,10 +86,8 @@ int loop(void *arg)
         } else if (event.filter == EVFILT_READ) {
             char buf[256];
             size_t readlen = ff_read(clientfd, buf, sizeof(buf));
-
             printf("bytes %zu are available to read...,data:%s,fd:%d\n", (size_t)event.data, buf, clientfd);
             ff_write(clientfd, html, sizeof(html));
-
         } else {
             printf("unknown event: %8.8X\n", event.flags);
         }
@@ -132,12 +123,7 @@ int main(int argc, char * argv[])
         exit(1);
     }
 
-    kevSet.data     = MAX_EVENTS;
-    kevSet.fflags   = 0;
-    kevSet.filter   = EVFILT_READ;
-    kevSet.flags    = EV_ADD;
-    kevSet.ident    = sockfd;
-    kevSet.udata    = NULL;
+    EV_SET(&kevSet, sockfd, EVFILT_READ, EV_ADD, 0, MAX_EVENTS, NULL);
 
     assert((kq = ff_kqueue()) > 0);
 
