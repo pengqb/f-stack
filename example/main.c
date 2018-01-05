d586d6320c01eebd97996c72b2bd29a03b7c05db
彭强兵 <pengqiangbing@pengqiangbingdeMacBook.local>
2018-01-05 16:26:29 +0800

    main.c surport multi port

--- example/main.c
+++ example/main.c
@@ -8,7 +8,8 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <assert.h>
-#include  <time.h>
+#include <stdbool.h>
+#include <time.h>
 
 #include "ff_config.h"
 #include "ff_api.h"
@@ -21,64 +22,46 @@ struct kevent kevSet;
 struct kevent events[MAX_EVENTS];
 /* kq */
 int kq;
-int sockfd;
-int nConn=0;
-int nReceiveMsg=0;
-char html[] =
-"HTTP/1.1 200 OK\r\n";
-//"Server: F-Stack\r\n"
-//"Date: Sat, 25 Feb 2017 09:26:33 GMT\r\n"
-//"Content-Type: text/html\r\n"
-//"Content-Length: 439\r\n"
-//"Last-Modified: Tue, 21 Feb 2017 09:44:03 GMT\r\n"
-//"Connection: keep-alive\r\n"
-//"Accept-Ranges: bytes\r\n"
-//"\r\n"
-//"<!DOCTYPE html>\r\n"
-//"<html>\r\n"
-//"<head>\r\n"
-//"<title>Welcome to F-Stack!</title>\r\n"
-//"<style>\r\n"
-//"    body {  \r\n"
-//"        width: 35em;\r\n"
-//"        margin: 0 auto; \r\n"
-//"        font-family: Tahoma, Verdana, Arial, sans-serif;\r\n"
-//"    }\r\n"
-//"</style>\r\n"
-//"</head>\r\n"
-//"<body>\r\n"
-//"<h1>Welcome to F-Stack!</h1>\r\n"
-//"\r\n"
-//"<p>For online documentation and support please refer to\r\n"
-//"<a href=\"http://F-Stack.org/\">F-Stack.org</a>.<br/>\r\n"
-//"\r\n"
-//"<p><em>Thank you for using F-Stack.</em></p>\r\n"
-//"</body>\r\n"
-//"</html>";
+int *sockfds;
+int nConn = 0;
+int nReceiveMsg = 0;
+int servPorts;
 
-int loop(void *arg)
-{
+char html[] = "HTTP/1.1 200 OK\r\n";
+
+bool isContains(int elem, int *container, int nElems) {
+    bool isContains = false;
+    for (int i = 0; i < nElems; i++) {
+        if (elem == container[i]) {
+            isContains = true;
+            break;
+        }
+    }
+    return isContains;
+}
+
+int loop(void *arg) {
     /* Wait for events to happen */
     unsigned nevents = ff_kevent(kq, NULL, 0, events, MAX_EVENTS, NULL);
     unsigned i;
 
     for (i = 0; i < nevents; ++i) {
         struct kevent event = events[i];
-        int clientfd = (int)event.ident;
+        int clientfd = (int) event.ident;
 
         /* Handle disconnect */
         if (event.flags & EV_EOF) {
             /* Simply close socket */
             ff_close(clientfd);
             nConn--;
-            if((nConn & 0xfff) == 0xfff){
-                printf("nConn=%d,time=%ld\n",nConn,time(NULL));
+            if ((nConn & 0xfff) == 0xfff) {
+                printf("nConn=%d,time=%ld\n", nConn, time(NULL));
             }
             //printf("A client has left the server...,fd:%d\n", clientfd);
-        } else if (clientfd == sockfd) {
-            int available = (int)event.data;
+        } else if (isContains(clientfd, sockfds, servPorts)) {
+            int available = (int) event.data;
             do {
-                int nclientfd = ff_accept(sockfd, NULL, NULL);
+                int nclientfd = ff_accept(clientfd, NULL, NULL);
                 if (nclientfd < 0) {
                     printf("ff_accept failed:%d, %s\n", errno,
                            strerror(errno));
@@ -88,14 +71,14 @@ int loop(void *arg)
                 /* Add to event list */
                 EV_SET(&kevSet, nclientfd, EVFILT_READ, EV_ADD, 0, 0, NULL);
 
-                if(ff_kevent(kq, &kevSet, 1, NULL, 0, NULL) < 0) {
+                if (ff_kevent(kq, &kevSet, 1, NULL, 0, NULL) < 0) {
                     printf("ff_kevent error:%d, %s\n", errno,
                            strerror(errno));
                     return -1;
                 }
                 nConn++;
-                if((nConn & 0xfff) == 0xfff){
-                    printf("nConn=%d,time=%ld\n",nConn,time(NULL));
+                if ((nConn & 0xfff) == 0xfff) {
+                    printf("nConn=%d,time=%ld\n", nConn, time(NULL));
                 }
                 //printf("A new client connected to the server..., fd:%d\n", nclientfd);
                 available--;
@@ -104,8 +87,8 @@ int loop(void *arg)
             char buf[256];
             size_t readlen = ff_read(clientfd, buf, sizeof(buf));
             nReceiveMsg++;
-            if((nReceiveMsg & 0x3fff) == 0x3fff){
-                printf("nReceiveMsg=%d,time=%ld\n",nReceiveMsg,time(NULL));
+            if ((nReceiveMsg & 0x3fff) == 0x3fff) {
+                printf("nReceiveMsg=%d,time=%ld\n", nReceiveMsg, time(NULL));
             }
             //printf("bytes %zu are available to read...,data:%s,fd:%d\n", (size_t)event.data, buf, clientfd);
             ff_write(clientfd, html, sizeof(html));
@@ -115,42 +98,45 @@ int loop(void *arg)
     }
 }
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
     ff_init(argc, argv);
 
-    sockfd = ff_socket(AF_INET, SOCK_STREAM, 0);
-    printf("sockfd:%d\n", sockfd);
-    if (sockfd < 0) {
-        printf("ff_socket failed\n");
-        exit(1);
-    }
-
-    struct sockaddr_in my_addr;
-    bzero(&my_addr, sizeof(my_addr));
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(80);
-    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    int ret = ff_bind(sockfd, (struct linux_sockaddr *)&my_addr, sizeof(my_addr));
-    if (ret < 0) {
-        printf("ff_bind failed\n");
-        exit(1);
-    }
-
-    ret = ff_listen(sockfd, MAX_EVENTS);
-    if (ret < 0) {
-        printf("ff_listen failed\n");
-        exit(1);
-    }
-
-    EV_SET(&kevSet, sockfd, EVFILT_READ, EV_ADD, 0, MAX_EVENTS, NULL);
-
+    int beginPort = 8010;
+    int endPort = 8090;
+    servPorts = endPort - beginPort;
+    int sockfdArray[servPorts];
     assert((kq = ff_kqueue()) > 0);
+    for (int i = 0; i < servPorts; i++) {
+        int sockfd = ff_socket(AF_INET, SOCK_STREAM, 0);
+        printf("sockfd:%d\n", sockfd);
+        if (sockfd < 0) {
+            printf("ff_socket failed\n");
+            exit(1);
+        }
 
-    /* Update kqueue */
-    ff_kevent(kq, &kevSet, 1, NULL, 0, NULL);
+        struct sockaddr_in my_addr;
+        bzero(&my_addr, sizeof(my_addr));
+        my_addr.sin_family = AF_INET;
+        my_addr.sin_port = htons(beginPort + i);
+        my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+        int ret = ff_bind(sockfd, (struct linux_sockaddr *) &my_addr, sizeof(my_addr));
+        if (ret < 0) {
+            printf("ff_bind failed\n");
+            exit(1);
+        }
+
+        ret = ff_listen(sockfd, MAX_EVENTS);
+        if (ret < 0) {
+            printf("ff_listen failed\n");
+            exit(1);
+        }
+        sockfdArray[i] = sockfd;
+        EV_SET(&kevSet, sockfd, EVFILT_READ, EV_ADD, 0, MAX_EVENTS, NULL);
+        /* Update kqueue */
+        ff_kevent(kq, &kevSet, 1, NULL, 0, NULL);
+    }
+    sockfds = sockfdArray;
     ff_run(loop, NULL);
     return 0;
 }
